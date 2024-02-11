177772e71fae3c8eb20bed68353c20b625c173b8
Andrew <telepnev.1997@mail.ru>
2024-02-11 16:04:11 +0700

    Initial commit

---
+++ src/test/java/com/example/GB_Eml1Sem4SpringCore/GbEml1Sem4SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GB_Eml1Sem4SpringCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbEml1Sem4SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
